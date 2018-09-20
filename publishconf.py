104bbc024a7cb78737f25d53dea4f2e2fe6ba6b3
Zhen Zhang <zhangzhen.dev@gmail.com>
2018-09-20 12:06:01 -0400

    change http to https

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'http://seclab-jhu.github.io/sec-read'
+SITEURL = 'https://seclab-jhu.github.io/sec-read'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
